49a5acebcdb18e4f52f182e607f2769272ef4f48
prakashdilli <prakashece245@gmail.com>
2022-10-15 22:57:00 +0530

    Add player movements

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,9 +13,12 @@ class Play extends Phaser.Scene {
         const map = this.createMap()
         const layers = this.createLayers(map)  
         
-        const player = this.createPlayer()
+        this.player = this.createPlayer()
 
-        this.physics.add.collider(player,layers.platformColliders)
+        this.playerSpeed = 200
+        this.physics.add.collider(this.player,layers.platformColliders)
+        
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     createMap() {
@@ -42,6 +45,19 @@ class Play extends Phaser.Scene {
         player.body.setGravityY(300)
         player.setCollideWorldBounds(true)
         return player;
+    }
+
+    update (){ // update methods called 60 times per sec
+        const { left, right } = this.cursors
+
+        if(left.isDown){
+            this.player.setVelocityX(-this.playerSpeed)
+        } else if(right.isDown){
+            this.player.setVelocityX(this.playerSpeed)
+        }
+        else{
+            this.player.setVelocityX(0)
+        }
     }
 }
 
