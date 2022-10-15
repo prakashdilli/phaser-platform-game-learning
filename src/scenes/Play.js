23f3ace276a3d0e44d94f0cf4d1858c030f11e48
prakashdilli <prakashece245@gmail.com>
2022-10-15 21:02:10 +0530

    little bit code refactoring

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,12 +10,23 @@ class Play extends Phaser.Scene {
     }
 
     create(){
-        const map = this.make.tilemap({key:'map'}) // key same as in preload title map name
-        const titleset1 = map.addTilesetImage('main_lev_build_1','title-1') // 1st arg asset name of the image used in the tilemap image, 2nd arg is the key same as in the preload
-        const titleset2 = map.addTilesetImage('main_lev_build_2','title-2') // 1st arg asset name of the image used in the tilemap image, 2nd arg is the key same as in the preload
+        const map = this.createMap()
+        const layers = this.createLayers(map)
+       
+    }
 
-        map.createStaticLayer('platform',titleset1) // 1st arg name of the layer in the tilemap, 2nd arg list of tileset used in that layer
-        map.createStaticLayer('environment',titleset1)
+    createMap(){
+        const map = this.make.tilemap({key:'map'}) // key same as in preload title map name
+        map.addTilesetImage('main_lev_build_1','title-1') // 1st arg asset name of the image used in the tilemap image, 2nd arg is the key same as in the preload
+        map.addTilesetImage('main_lev_build_2','title-2') // 1st arg asset name of the image used in the tilemap image, 2nd arg is the key same as in the preload
+        return map;
+    }
+
+    createLayers(map){
+        const titleset = map.getTileset('main_lev_build_1')
+        const environment = map.createStaticLayer('environment',titleset)
+        const platform =  map.createStaticLayer('platform',titleset) // 1st arg name of the layer in the tilemap, 2nd arg list of tileset used in that layer
+        return { environment,platform }
     }
 }
 
